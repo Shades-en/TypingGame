d0a69697b16e9edc40227b96fcc80e381d9cff29
Owais Iqbal <owaisiqbal2013@gmail.com>
2022-03-23 16:33:34 +0530

    update

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+if (window.matchMedia('(max-width: 768px)').matches){
+    while(true)
+        alert("Mobile alert: Cannot view this application in Mobiles")
+}
+
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
 let totalWords=0;
 for(i=0;i<sentences.length;i++){
     totalWords+=sentences[i].split(" ").length
 }
 
-alert("hello")
 $("div#keyboard-upper-container").hide();
-
-$('#buscar-producto').on('keydown', function(e){
-    var key = console.log(e.which)
-});
 
 $(document).keydown(function(e){
     if (e.shiftKey){
         $("div#keyboard-upper-container").show();
-        $("div#keyboard-lower-container").hide();
-        
+        $("div#keyboard-lower-container").hide();   
     }
 }
 );
@@ -23,7 +23,7 @@ $(document).keydown(function(e){
 $(document).keyup(function(e){
     $("div#keyboard-upper-container").hide();
     $("div#keyboard-lower-container").show();
-    $("span,div#32").css("background-color","inherit");
+    $("#keys span ,div#32").css("background-color","inherit");
 }
 );
         
@@ -53,14 +53,16 @@ $("div#target-letter button").css({
     "font-size":"14px",
     "padding":"5px"
 });
-$("div#yellow-block").css("opacity","0");
 $("div#target-letter button").click(game);
 
 function game(){
     $("div#sentence").empty();
-    $("div#sentence").append(sentences[0]);
+
+    let markedSentence=sentences[x].split("")
+    markedSentence[0]=`<span id="highlight">${markedSentence[0]}</span>`
+    $("div#sentence").append(markedSentence.join(""));
+
     $("div#target-letter").empty();
-    $("div#yellow-block").css("opacity","1");
     x=0;
     y=0;
     $("div#target-letter").append(sentences[x][y]);
@@ -68,8 +70,12 @@ function game(){
 
 function newSentence(x,start){
     $("div#sentence").empty();
-    if(x<sentences.length)
-        $("div#sentence").append(sentences[x]);
+    if(x<sentences.length){
+        let markedSentence=sentences[x].split("")
+        markedSentence[0]=`<span id="highlight">${markedSentence[0]}</span>`
+        $("div#sentence").append(markedSentence.join(""));
+    }
+        
     else{
         $("div#target-letter").empty();
         let min=(Date.now()-start)/60000;
@@ -85,7 +91,6 @@ function newSentence(x,start){
             "font-size":"14px",
             "padding":"5px"
         });
-        $("div#yellow-block").css("opacity","0");
         $("div#target-letter button").click(game);
     }
     $("div#feedback").empty();
@@ -98,10 +103,18 @@ $(document).keypress(function(e){
             start = Date.now();
             if(e.which==sentences[x][y].charCodeAt(0)){
                 $("div#feedback").append("✔");
-                $("div#yellow-block").css("left","+=17.5px");
+
+                $("div#sentence").text("")
+                let markedSentence=sentences[x].split("")
+                markedSentence[y+1]=`<span id="highlight">${markedSentence[y+1]}</span>`
+                $("div#sentence").append(markedSentence.join(""));
+
                 $("div#target-letter").empty();
                 if(y<sentences[x].length)
-                    $("div#target-letter").append(sentences[x][y+1]);
+                    if(sentences[x][y+1]==' ')
+                        $("div#target-letter").append("#space");
+                    else
+                        $("div#target-letter").append(sentences[x][y+1]);
                 y++;
             }
             else{
@@ -115,7 +128,6 @@ $(document).keypress(function(e){
                     }
                 }
                 newSentence(++x, start);
-                $("div#yellow-block").css("left","90px");
                 y=0;
                 if(x<sentences.length)
                     $("div#target-letter").append(sentences[x][y]);
