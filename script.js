aa860c16c6f249f6c573d7c7fc79f42c211a4185
Owais Iqbal <owaisiqbal2013@gmail.com>
2022-03-24 02:28:53 +0530

    Complete

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ if (window.matchMedia('(max-width: 768px)').matches){
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
 let totalWords=0;
-for(i=0;i<sentences.length;i++){
-    totalWords+=sentences[i].split(" ").length
-}
+
 
 $("div#keyboard-upper-container").hide();
+$("#target").hide();
 
 $(document).keydown(function(e){
     if (e.shiftKey){
@@ -30,33 +29,34 @@ $(document).keyup(function(e){
 $(document).keypress(function(e){
     for(i=0;i<$("span").length;i++){
         if(e.which==$("span")[i].id){
-            $("span#"+$("span")[i].id).css("background-color","yellow");
+            $("span#"+$("span")[i].id).css("background-color","#f5b5d3f5");
         }
         else if(e.which=="32"){
-            $("div#32").css("background-color","yellow")
+            $("div#32").css("background-color","#f5b5d3f5")
         }
     }
 });
-
 
 var x=0;
 var y=0;
 var totalwrong=0;
 var start=null;
+var space=false
 
-$("div#sentence").append("Play the typing game and know your speed!!");
-$("div#target-letter").append("<button>Play</button>");
-$("div#target-letter button").css({
-    "background-color":"green",
-    "color":"white",
-    "border-radius":"5px",
-    "font-size":"14px",
-    "padding":"5px"
+$(".plays button").click(()=>{
+    game()
+    $("#target").show()
+    $(".content").hide()
+    $("#words").hide()
 });
-$("div#target-letter button").click(game);
 
 function game(){
+    for(i=0;i<sentences.length;i++){
+        totalWords+=sentences[i].split(" ").length
+    }
+    space=true
     $("div#sentence").empty();
+    $("div#feedback").empty();
 
     let markedSentence=sentences[x].split("")
     markedSentence[0]=`<span id="highlight">${markedSentence[0]}</span>`
@@ -65,7 +65,7 @@ function game(){
     $("div#target-letter").empty();
     x=0;
     y=0;
-    $("div#target-letter").append(sentences[x][y]);
+    $("div#target-letter").append(`<kbd>${sentences[x][y]}</kbd>`);
 }
 
 function newSentence(x,start){
@@ -81,15 +81,17 @@ function newSentence(x,start){
         let min=(Date.now()-start)/60000;
         let letters= (totalWords/min)-(2*totalwrong);
         $("div#sentence").empty();
-        $("div#sentence").append("Ran out of Sentences! Word count per minute: "+letters);
+        $("div#sentence").append("Ran out of Sentences! <br> Word count per minute: "+letters);
         $("div#target-letter").empty();
-        $("div#target-letter").append("<button>Wanna play again?</button>");
+        $("div#target-letter").append("<button>Try again?</button>");
         $("div#target-letter button").css({
-            "background-color":"green",
-            "color":"white",
-            "border-radius":"5px",
-            "font-size":"14px",
-            "padding":"5px"
+            "background-color":"#f5b5d3f5",
+            "color":"black",
+            "border-radius":"10px",
+            "padding":"10px",
+            "font-size":"15px",
+            "border":"none",
+            "font-family": "'archivoregular', sans-serif"
         });
         $("div#target-letter button").click(game);
     }
@@ -97,6 +99,9 @@ function newSentence(x,start){
 }
 
 $(document).keypress(function(e){
+    if(e.keyCode == 32 && space) {
+        e.preventDefault();
+    }
     if(x<sentences.length){
         if($("div#target-letter button").length==0){
             if(start==null)
@@ -112,9 +117,10 @@ $(document).keypress(function(e){
                 $("div#target-letter").empty();
                 if(y<sentences[x].length)
                     if(sentences[x][y+1]==' ')
-                        $("div#target-letter").append("#space");
-                    else
-                        $("div#target-letter").append(sentences[x][y+1]);
+                        $("div#target-letter").append("<kbd>space</kbd>");
+                    else{
+                        $("div#target-letter").append(`<kbd>${sentences[x][y+1]}</kbd>`);
+                    }
                 y++;
             }
             else{
@@ -129,8 +135,10 @@ $(document).keypress(function(e){
                 }
                 newSentence(++x, start);
                 y=0;
-                if(x<sentences.length)
-                    $("div#target-letter").append(sentences[x][y]);
+                if(x<sentences.length){
+                    $("div#target-letter").text("");
+                    $("div#target-letter").append(`<kbd>${sentences[x][y]}</kbd>`);
+                }
                 else{
                     totalwrong=0;
                     start=null;
@@ -141,4 +149,17 @@ $(document).keypress(function(e){
         }
     }
 });
+
+$(".logo").click(()=>{
+    location.reload()
+})
+
+$("#current").click(()=>$("textarea").val(sentences.join("\n")))
+$("#confirm").click(()=>{
+    sentences=$("textarea").val().split(", ")
+    $("textarea").val("")
+})
+
+
+
 
